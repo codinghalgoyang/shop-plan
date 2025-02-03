a6a120764c9e8f845f3d3c16c7070150a17445c9
codinghalgoyang <codinghalgoyang@gmail.com>
2025-02-03 19:56:29 +0900

    user session 정보 확인
    Signed-off-by: codinghalgoyang <codinghalgoyang@gmail.com>

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,35 @@
 import {
   GoogleSignin,
   GoogleSigninButton,
-  SignInResponse,
+  User,
 } from "@react-native-google-signin/google-signin";
 import { useEffect, useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function IndexScreen() {
   const [error, setError] = useState<Error | null>(null);
-  const [userInfo, setUserInfo] = useState<SignInResponse | null>(null);
+  const [userInfo, setUserInfo] = useState<User | null>(null);
 
-  useEffect(() => {
-    GoogleSignin.configure({
-      webClientId: process.env.GOOGLE_SIGN_IN_CLIENT_ID,
-    });
-  }, []);
+  const checkUserSession = async () => {
+    try {
+      const userInfo = await GoogleSignin.getCurrentUser();
+      if (userInfo) {
+        console.log("사용자가 로그인 상태입니다:", userInfo);
+        setUserInfo(userInfo);
+      } else {
+        console.log("사용자가 로그인하지 않았습니다.");
+      }
+    } catch (error) {
+      console.error("세션 확인 중 오류 발생:", error);
+    }
+  };
 
   const signin = async () => {
     try {
       await GoogleSignin.hasPlayServices();
-      const user = await GoogleSignin.signIn();
-      setUserInfo(user);
+      const response = await GoogleSignin.signIn();
+      const userInfo = response.data;
+      setUserInfo(userInfo);
       setError(null);
     } catch (e) {
       setError(e as Error);
@@ -33,10 +42,17 @@ export default function IndexScreen() {
     GoogleSignin.signOut();
   };
 
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: process.env.GOOGLE_SIGN_IN_CLIENT_ID,
+    });
+    checkUserSession();
+  }, []);
+
   return (
     <View style={styles.container}>
       {error && <Text>error : {JSON.stringify(error)}</Text>}
-      {userInfo && <Text>{JSON.stringify(userInfo.data?.user)}</Text>}
+      {userInfo && <Text>{JSON.stringify(userInfo.user)}</Text>}
       {userInfo ? (
         <Button title="Logout" onPress={logout} />
       ) : (
