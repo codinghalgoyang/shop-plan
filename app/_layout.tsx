2352fa32ca4217e20b11226b2f69b5b95ea142ae
codinghalgoyang <codinghalgoyang@gmail.com>
2025-02-03 20:58:13 +0900

    SafeAreaView로 감싸기
    Signed-off-by: codinghalgoyang <codinghalgoyang@gmail.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,38 @@
 import { Stack } from "expo-router";
+import { SafeAreaView, StyleSheet } from "react-native";
 import { RecoilRoot } from "recoil";
 
 export default function RootLayout() {
   return (
     <RecoilRoot>
-      <Stack>
-        <Stack.Screen
-          name="home"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="error"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="login"
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack>
+      <SafeAreaView style={styles.container}>
+        <Stack>
+          <Stack.Screen
+            name="home"
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="error"
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="login"
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack>
+      </SafeAreaView>
     </RecoilRoot>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
