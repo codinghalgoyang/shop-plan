db12a11f140c786e82a55cdb4a23824376e3c757
codinghalgoyang <codinghalgoyang@gmail.com>
2025-02-03 19:17:08 +0900

    Use firebase & google signin
    Signed-off-by: codinghalgoyang <codinghalgoyang@gmail.com>

---
+++ app.config.js
@@ -0,0 +1,58 @@
+export default {
+  expo: {
+    name: "shop-plan",
+    slug: "shop-plan",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.codinghalgoyang.shopplan",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
+      googleServiceFile: process.env.GOOGLE_SERVICES_INFOPLIST,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.codinghalgoyang.shopplan",
+      googleServiceFile: process.env.GOOGLE_SERVICES_JSON,
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      "@react-native-google-signin/google-signin",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {
+        origin: false,
+      },
+      eas: {
+        projectId: "d71e023d-ed5e-4348-9443-b5a0ec3ee4b3",
+      },
+    },
+    owner: "codinghalgoyang",
+  },
+};
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,52 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  SignInResponse,
+} from "@react-native-google-signin/google-signin";
+import { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function IndexScreen() {
+  const [error, setError] = useState<Error | null>(null);
+  const [userInfo, setUserInfo] = useState<SignInResponse | null>(null);
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        "729367066840-t6c7vbdd5p8hdt2phma8f9caqch64d13.apps.googleusercontent.com",
+    });
+  }, []);
+
+  const signin = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const user = await GoogleSignin.signIn();
+      setUserInfo(user);
+      setError(null);
+    } catch (e) {
+      setError(e as Error);
+    }
+  };
+
+  const logout = () => {
+    setUserInfo(null);
+    GoogleSignin.revokeAccess();
+    GoogleSignin.signOut();
+  };
+
   return (
     <View style={styles.container}>
+      {error && <Text>error : {JSON.stringify(error)}</Text>}
+      {userInfo && <Text>{JSON.stringify(userInfo.data?.user)}</Text>}
+      {userInfo ? (
+        <Button title="Logout" onPress={logout} />
+      ) : (
+        <GoogleSigninButton
+          size={GoogleSigninButton.Size.Standard}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={signin}
+        />
+      )}
       <Text>Index Screen</Text>
     </View>
   );
