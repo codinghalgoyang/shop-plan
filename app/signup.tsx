e212daee0a03e4fd3f6ddbd54e83963cb8bc5079
codinghalgoyang <codinghalgoyang@gmail.com>
2025-02-10 20:22:28 +0900

    Input username
    Signed-off-by: codinghalgoyang <codinghalgoyang@gmail.com>

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,10 +1,44 @@
+import { googleUserState } from "@/atoms/googleUserAtom";
+import Header from "@/components/Header";
 import ScreenView from "@/components/ScreenView";
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, Button, TextInput } from "react-native";
+import { useRecoilValue } from "recoil";
 
 export default function SignupScreen() {
+  const googleUser = useRecoilValue(googleUserState);
+  const [username, setUsername] = useState("");
+
+  const signup = () => {
+    if (!username) {
+      console.log("Input username");
+      return;
+    }
+    console.log("sign up with ", username);
+  };
+
   return (
     <ScreenView>
-      <Text>Signup Screen</Text>
+      <Header title="SignUp" />
+      <Text>username</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="username"
+        value={username}
+        onChangeText={setUsername}
+        autoCapitalize="none" // 자동 대문자 막기
+      />
+      <Button title={"Sign Up"} onPress={signup} />
     </ScreenView>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+});
