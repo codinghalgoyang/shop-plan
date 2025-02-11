5bf0eef992bebbee5fc1cb3943276eacda04f6d8
codinghalgoyang <codinghalgoyang@gmail.com>
2025-02-11 22:15:55 +0900

    Add title & users to AddPlanScreen
    Signed-off-by: codinghalgoyang <codinghalgoyang@gmail.com>

--- app/add_plan.tsx
+++ app/add_plan.tsx
@@ -1,12 +1,65 @@
+import { shopPlanUserState } from "@/atoms/shopPlanUserAtom";
 import Header from "@/components/Header";
 import ScreenView from "@/components/ScreenView";
-import { Text } from "react-native";
+import { ShopPlanUser } from "@/utils/types";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import { useRecoilValue } from "recoil";
 
 export default function AddPlanScreen() {
+  const shopPlanUser = useRecoilValue(shopPlanUserState);
+  const [title, setTitle] = useState("");
+  const [users, setUsers] = useState<ShopPlanUser[]>([]);
+
+  useEffect(() => {
+    if (shopPlanUser) {
+      setUsers([shopPlanUser]);
+    }
+  }, []);
+
   return (
     <ScreenView>
       <Header title="AddPlan" enableBackAction />
-      <Text>Add Plan</Text>
+      <View>
+        <Text>Title</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="title"
+          value={title}
+          onChangeText={setTitle}
+          autoCapitalize="none" // 자동 대문자 막기
+        />
+      </View>
+      <View>
+        <Text>Users</Text>
+        <ScrollView>
+          {users.map((user) => {
+            return (
+              <View key={user.uid}>
+                <Text>{user.username}</Text>
+              </View>
+            );
+          })}
+        </ScrollView>
+      </View>
+      <Button title="Add Plan" />
     </ScreenView>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+});
