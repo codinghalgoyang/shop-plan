ffc9e6a5d64d6b7b439065c02b804a45148305a9
codinghalgoyang <codinghalgoyang@gmail.com>
2025-02-14 15:57:18 +0900

    subscribe plan in PlanScreen
    Signed-off-by: codinghalgoyang <codinghalgoyang@gmail.com>

--- app/plan.tsx
+++ app/plan.tsx
@@ -1,14 +1,53 @@
 import Header from "@/components/Header";
 import ScreenView from "@/components/ScreenView";
+import { db } from "@/utils/firebaseConfig";
+import { Plan } from "@/utils/types";
 import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
 import { Text } from "react-native";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  onSnapshot,
+} from "firebase/firestore";
 
 export default function PlanScreen() {
   const { planId } = useLocalSearchParams();
+  const [plan, setPlan] = useState<Plan>();
+
+  // subscribe planDoc
+  useEffect(() => {
+    if (!planId) return;
+
+    const planDocRef = doc(
+      db,
+      "Plans",
+      Array.isArray(planId) ? planId[0] : planId
+    );
+    const unsubscribe = onSnapshot(
+      planDocRef,
+      (planDoc) => {
+        if (planDoc.exists()) {
+          const newPlan = planDoc.data() as Plan;
+          setPlan(newPlan);
+        } else {
+          console.log("No such plan : ", planId);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    return unsubscribe;
+  }, [planId]);
+
   return (
     <ScreenView>
-      <Header title="Plan" enableBackAction />
-      <Text>PlanScreen : {planId}</Text>
+      <Header title={plan ? plan.title : "Loading..."} enableBackAction />
+      <Text>PlanScreen : {plan?.title}</Text>
     </ScreenView>
   );
 }
