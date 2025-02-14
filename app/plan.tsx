27376e60c12f15b56eed35c79691dbf305cad458
codinghalgoyang <codinghalgoyang@gmail.com>
2025-02-14 15:28:04 +0900

    Add header to PlanScreen
    Signed-off-by: codinghalgoyang <codinghalgoyang@gmail.com>

--- app/plan.tsx
+++ app/plan.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import ScreenView from "@/components/ScreenView";
 import { useLocalSearchParams } from "expo-router";
 import { Text } from "react-native";
@@ -6,6 +7,7 @@ export default function PlanScreen() {
   const { planId } = useLocalSearchParams();
   return (
     <ScreenView>
+      <Header title="Plan" enableBackAction />
       <Text>PlanScreen : {planId}</Text>
     </ScreenView>
   );
