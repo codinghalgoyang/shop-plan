8f284932853ff41e630878f97271a22396d75859
codinghalgoyang <codinghalgoyang@gmail.com>
2025-02-18 22:02:59 +0900

    Change title
    Signed-off-by: codinghalgoyang <codinghalgoyang@gmail.com>

--- app/modify_plan.tsx
+++ app/modify_plan.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import ModifyPlanMembersView from "@/components/ModifyPlan/ModifyPlanMembersView";
 import ScreenView from "@/components/ScreenView";
 import { firestoreUpdatePlan } from "@/utils/api";
+import { Plan } from "@/utils/types";
 import { param2string } from "@/utils/utils";
 import { router, useLocalSearchParams } from "expo-router";
 import { useState } from "react";
@@ -19,24 +20,41 @@ export default function ModifyPlanScreen() {
   const [title, setTitle] = useState(plan.title);
   const user = useRecoilValue(userState);
   const planUser = plan.planUsers.find((planUser) => planUser.uid === user.uid);
+  const editable = planUser?.isAdmin;
+
+  const changeTitle = async () => {
+    const newPlan: Plan = { ...plan };
+    newPlan.title = title;
+    await firestoreUpdatePlan(newPlan);
+    console.log("Change the title : ", title);
+  };
 
   return (
     <ScreenView>
       <Header title="ModifyPlan" enableBackAction />
       <View>
         <Text>Title</Text>
-        <TextInput
-          style={[
-            styles.input,
-            { color: planUser?.isAdmin ? "black" : "gray" },
-            { borderWidth: planUser?.isAdmin ? 1 : 0 },
-          ]}
-          placeholder="title"
-          value={title}
-          onChangeText={setTitle}
-          autoCapitalize="none" // 자동 대문자 막기
-          editable={planUser?.isAdmin}
-        />
+        <View style={styles.titleInputContainer}>
+          <TextInput
+            style={[
+              styles.input,
+              { color: editable ? "black" : "gray" },
+              { borderWidth: editable ? 1 : 0 },
+            ]}
+            placeholder="title"
+            value={title}
+            onChangeText={setTitle}
+            autoCapitalize="none" // 자동 대문자 막기
+            editable={editable}
+          />
+          {editable && (
+            <Button
+              title="변경"
+              disabled={title === plan.title}
+              onPress={changeTitle}
+            />
+          )}
+        </View>
       </View>
       <ModifyPlanMembersView plan={plan} />
     </ScreenView>
@@ -45,10 +63,15 @@ export default function ModifyPlanScreen() {
 
 const styles = StyleSheet.create({
   input: {
-    height: 40,
     fontSize: 24,
     marginBottom: 12,
     paddingHorizontal: 8,
+    flex: 1,
+  },
+  titleInputContainer: {
+    flexDirection: "row",
+    gap: 5,
+    alignItems: "center",
   },
   buttonContainer: {
     marginTop: 5,
