6586d9c79981089190dd7190ce5416f6d4e770cf
codinghalgoyang <codinghalgoyang@gmail.com>
2025-03-07 16:15:31 +0900

    Change main colors
    Signed-off-by: codinghalgoyang <codinghalgoyang@gmail.com>

--- utils/Colors.ts
+++ utils/Colors.ts
@@ -1,18 +1,23 @@
 export const Colors = {
-  primary: "rgb(6, 168, 97)",
-  notification: "rgb(168, 38, 0)",
-  border: "rgb(225, 225, 225)",
+  primary: "rgb(250, 80, 57)",
+  secondary: "rgb(31, 141, 249)",
+  border: "rgb(243, 243, 243)",
   background: {
     white: "rgb(255, 255, 255)",
-    superLightGray: "rgb(249, 249, 249)",
-    lightGray: "rgb(247, 247, 247)", // input
-    disabled: "rgb(151, 151, 151)",
-    black: "rgb(45, 41, 39)",
+    lightGray: "rgb(250, 250, 250)", // to make white paper popup
+    gray: "rgb(243, 243, 243)", // debossing background
+    black: "rgb(42, 42, 42)",
   },
   content: {
+    // for bg White or light gray
+    primary: "rgb(60, 60, 60)",
+    sencondary: "rgb(150, 150, 150)",
+    // for black or primary background
     white: "rgb(255, 255, 255)",
-    placeholder: "rgb(218, 218, 218)",
-    secondary: "rgb(165, 165, 165)",
-    primary: "rgb(33, 33, 33)",
+    // for bg Gray
+    bgGray: {
+      primary: "rgb(87, 87, 87)",
+      secondary: "rgb(185, 185, 185)",
+    },
   },
 };
