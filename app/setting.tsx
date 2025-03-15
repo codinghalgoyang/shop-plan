10b8d1d364ef7ff2b9a0ae4889813aa5d4bdf5f7
codinghalgoyang <codinghalgoyang@gmail.com>
2025-03-15 11:11:16 +0900

    delete user
    Signed-off-by: codinghalgoyang <codinghalgoyang@gmail.com>

--- app/setting.tsx
+++ app/setting.tsx
@@ -4,25 +4,11 @@ import ScreenView from "@/components/Common/ScreenView";
 import SettingItemUser from "@/components/Setting/SettingItemUser";
 import SettingItemAOD from "@/components/Setting/SettingItemAOD";
 import { Colors } from "@/utils/Colors";
-import { useRecoilState } from "recoil";
-import { defaultUser, userState } from "@/atoms/userAtom";
-import { GoogleSignin } from "@react-native-google-signin/google-signin";
-import { router } from "expo-router";
 import SettingItemTermsOfUse from "@/components/Setting/SettingItemTermsOfUse";
 import SettingItemDeleteUser from "@/components/Setting/SettingItemDeleteUser";
 import ThemedText from "@/components/Common/ThemedText";
 
 export default function SettingScreen() {
-  const [user, setUser] = useRecoilState(userState);
-
-  const logout = () => {
-    setUser(defaultUser);
-    GoogleSignin.revokeAccess();
-    GoogleSignin.signOut();
-    router.dismissAll();
-    router.replace("/login");
-  };
-
   return (
     <ScreenView>
       <Header title="설정" enableBackAction={true} />
@@ -31,6 +17,7 @@ export default function SettingScreen() {
           회원 정보
         </ThemedText>
         <SettingItemUser />
+        <SettingItemDeleteUser />
         <ThemedText size="small" color="gray" style={{ marginLeft: 8 }}>
           기타
         </ThemedText>
--- components/Setting/SettingItemDeleteUser.tsx
+++ components/Setting/SettingItemDeleteUser.tsx
@@ -4,22 +4,38 @@ import { Colors } from "@/utils/Colors";
 import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
 import ThemedIcon from "../Common/ThemedIcon";
 import { router } from "expo-router";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { defaultUser, userState } from "@/atoms/userAtom";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { plansState } from "@/atoms/plansAtom";
+import { invitedPlansState } from "@/atoms/invitedPlanAtom";
+import { modalState } from "@/atoms/modalAtom";
 
 export default function SettingItemDeleteUser() {
   const [user, setUser] = useRecoilState(userState);
+  const setModal = useSetRecoilState(modalState);
+  const plans = useRecoilValue(plansState);
+  const invitedPlans = useRecoilValue(invitedPlansState);
+
+  const deleteUser = () => {
+    if (plans.length !== 0 || invitedPlans.length !== 0) {
+      setModal({
+        visible: true,
+        message:
+          "나의 플랜과 초대받은 플랜을 모두 정리 후 회원탈퇴가 가능합니다.",
+      });
+      return;
+    }
+
+    setUser(defaultUser);
+    GoogleSignin.revokeAccess();
+    GoogleSignin.signOut();
+    router.dismissAll();
+    router.push("/login");
+  };
+
   return (
-    <TouchableOpacity
-      onPress={() => {
-        setUser(defaultUser);
-        GoogleSignin.revokeAccess();
-        GoogleSignin.signOut();
-        router.dismissAll();
-        router.push("/login");
-      }}
-    >
+    <TouchableOpacity onPress={deleteUser}>
       <View style={styles.container}>
         <ThemedText>회원탈퇴</ThemedText>
         <ThemedIcon IconComponent={SimpleLineIcons} iconName="arrow-right" />
