5561d1dcce5f3da2a35aceecdf6f216e8880cf74
codinghalgoyang <codinghalgoyang@gmail.com>
2025-03-25 22:23:39 +0900

    Fix delete plan bug
    Signed-off-by: codinghalgoyang <codinghalgoyang@gmail.com>

--- app/edit_plan.tsx
+++ app/edit_plan.tsx
@@ -26,21 +26,19 @@ export default function EditPlanScreen() {
   const { plan_id: planId } = useLocalSearchParams();
   const plans = useRecoilValue(plansState);
   const plan = plans.find((plan) => plan.id === planId);
-  if (plan === undefined) {
-    router.back();
-    return null;
-  }
-
-  const [title, setTitle] = useState(plan.title);
+  const [title, setTitle] = useState(plan?.title);
   const user = useRecoilValue(userState);
-  const myPlanUser = plan.planUsers.find(
+  const myPlanUser = plan?.planUsers.find(
     (planUser) => planUser.uid === user.uid
   ) ?? { uid: "", username: "Unknown user", isAdmin: false };
-  const admins = plan.planUsers.filter((planUser) => planUser.isAdmin);
+  const admins = plan?.planUsers.filter((planUser) => planUser.isAdmin);
   const editable = myPlanUser?.isAdmin;
   const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
 
   const changeTitle = async () => {
+    if (!plan) return;
+    if (!title) return;
+
     setModal({
       visible: true,
       message: `플랜 제목을 '${title}' 로 변경하시겠습니까?`,
@@ -60,6 +58,7 @@ export default function EditPlanScreen() {
   };
 
   const removePlan = async () => {
+    if (!plan) return;
     setModal({
       visible: true,
       message: `정말 '${plan.title}' 플랜을 삭제하시겠습니까?`,
@@ -79,6 +78,7 @@ export default function EditPlanScreen() {
   };
 
   const escapePlan = async () => {
+    if (!plan) return;
     setModal({
       visible: true,
       message: `정말 '${plan.title}' 플랜을 나가시겠습니까?`,
@@ -125,65 +125,67 @@ export default function EditPlanScreen() {
   return (
     <ScreenView>
       <Header title="플랜 편집" enableBackAction />
-      <View style={styles.container}>
-        <ThemedText>플랜 제목</ThemedText>
-        <View style={styles.titleContainer}>
-          <ThemedTextInput
-            placeholder="플랜 제목 입력"
-            value={title}
-            onChangeText={setTitle}
-            autoCapitalize="none" // 자동 대문자 막기
-            editable={editable}
-            style={styles.input}
-          />
-          {editable && (
-            <ThemedTextButton
-              disabled={title == plan.title || title == ""}
-              onPress={changeTitle}
-              color={title == plan.title || title == "" ? "gray" : "blue"}
-              type="fill"
-            >
-              변경
-            </ThemedTextButton>
-          )}
-        </View>
-        <ThemedText>사용자</ThemedText>
-        <EditPlanMembersView plan={plan} />
-        {!isKeyboardOpen && (
-          <View style={styles.buttonContainer}>
-            {(admins.length > 1 || !myPlanUser.isAdmin) && (
+      {plan !== undefined && admins?.length !== 0 && (
+        <View style={styles.container}>
+          <ThemedText>플랜 제목</ThemedText>
+          <View style={styles.titleContainer}>
+            <ThemedTextInput
+              placeholder="플랜 제목 입력"
+              value={title}
+              onChangeText={setTitle}
+              autoCapitalize="none" // 자동 대문자 막기
+              editable={editable}
+              style={styles.input}
+            />
+            {editable && (
               <ThemedTextButton
-                onPress={escapePlan}
-                buttonStyle={styles.button}
-                weight="bold"
-                color="orange"
-                type={myPlanUser.isAdmin ? "outline" : "fill"}
-              >
-                플랜 나가기
-              </ThemedTextButton>
-            )}
-            {myPlanUser.isAdmin && (
-              <ThemedTextButton
-                onPress={removePlan}
-                buttonStyle={styles.button}
-                color="orange"
-                weight="bold"
+                disabled={title == plan.title || title == ""}
+                onPress={changeTitle}
+                color={title == plan.title || title == "" ? "gray" : "blue"}
                 type="fill"
               >
-                플랜 삭제하기
+                변경
               </ThemedTextButton>
             )}
-            <ThemedTextButton
-              onPress={exitEdit}
-              buttonStyle={styles.button}
-              color="black"
-              type="plain"
-            >
-              편집 종료
-            </ThemedTextButton>
           </View>
-        )}
-      </View>
+          <ThemedText>사용자</ThemedText>
+          <EditPlanMembersView plan={plan} />
+          {!isKeyboardOpen && (
+            <View style={styles.buttonContainer}>
+              {((admins && admins.length > 1) || !myPlanUser.isAdmin) && (
+                <ThemedTextButton
+                  onPress={escapePlan}
+                  buttonStyle={styles.button}
+                  weight="bold"
+                  color="orange"
+                  type={myPlanUser.isAdmin ? "outline" : "fill"}
+                >
+                  플랜 나가기
+                </ThemedTextButton>
+              )}
+              {myPlanUser.isAdmin && (
+                <ThemedTextButton
+                  onPress={removePlan}
+                  buttonStyle={styles.button}
+                  color="orange"
+                  weight="bold"
+                  type="fill"
+                >
+                  플랜 삭제하기
+                </ThemedTextButton>
+              )}
+              <ThemedTextButton
+                onPress={exitEdit}
+                buttonStyle={styles.button}
+                color="black"
+                type="plain"
+              >
+                편집 종료
+              </ThemedTextButton>
+            </View>
+          )}
+        </View>
+      )}
     </ScreenView>
   );
 }
