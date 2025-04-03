c34a5aea3e5aa80254a1bef801faeb8b19ec017f
codinghalgoyang <codinghalgoyang@gmail.com>
2025-04-03 17:35:12 +0900

    Seperate PlanItemsView
    Signed-off-by: codinghalgoyang <codinghalgoyang@gmail.com>

--- app/plan.tsx
+++ app/plan.tsx
@@ -11,7 +11,7 @@ import PlanItemInput from "@/components/Plan/PlanItemInput";
 import { Plan } from "@/utils/types";
 import ThemedText from "@/components/Common/ThemedText";
 import Paper from "@/components/Common/Paper";
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import PlanItemEdit from "@/components/Plan/PlanItemEdit";
 import ThemedTextButton from "@/components/Common/ThemedTextButton";
 import {
@@ -21,6 +21,7 @@ import {
 import { modalState } from "@/atoms/modalAtom";
 import PlanHeader from "@/components/Plan/PlanHeader";
 import PlanCoupangButton from "@/components/Plan/PlanCoupanButton";
+import PlanItemsView from "@/components/Plan/PlanItemsView";
 
 function getCategories(plan: Plan) {
   const allCategories = plan?.items.map((item) => item.category);
@@ -56,59 +57,13 @@ export default function PlanScreen() {
       />
       <View style={styles.container}>
         {!isDeleteMode && <PlanCoupangButton />}
-        <ScrollView contentContainerStyle={{ gap: 8 }}>
-          {categories.map((category) => {
-            return (
-              <View key={category} style={{ gap: 8 }}>
-                <ThemedText
-                  size="small"
-                  color="gray"
-                  style={{ marginLeft: 12 }}
-                >
-                  {categories.length == 1 && categories[0] == ""
-                    ? "구매 항목"
-                    : category == ""
-                    ? "분류 없음"
-                    : category}
-                </ThemedText>
-                <Paper>
-                  {plan?.items.map((planItem, itemIdx) => {
-                    if (planItem.checked) return null;
-                    if (planItem.category !== category) return null;
-                    return (
-                      <PlanItemView
-                        key={planItem.title + planItem.createdAt}
-                        plan={plan}
-                        itemIdx={itemIdx}
-                        needTopBorder={itemIdx == 0}
-                        editItemIdx={editItemIdx}
-                        setEditItemIdx={setEditItemIdx}
-                        isDeleteMode={isDeleteMode}
-                      />
-                    );
-                  })}
-                  {plan?.items.map((planItem, itemIdx) => {
-                    if (!planItem.checked) return null;
-                    if (planItem.category !== category) return null;
-                    return (
-                      <PlanItemView
-                        key={planItem.title + planItem.createdAt}
-                        plan={plan}
-                        itemIdx={itemIdx}
-                        needTopBorder={itemIdx == 0}
-                        editItemIdx={editItemIdx}
-                        setEditItemIdx={setEditItemIdx}
-                        isDeleteMode={isDeleteMode}
-                      />
-                    );
-                  })}
-                </Paper>
-              </View>
-            );
-          })}
-        </ScrollView>
+        <PlanItemsView
+          plan={plan}
+          editItemIdx={editItemIdx}
+          setEditItemIdx={setEditItemIdx}
+          isDeleteMode={isDeleteMode}
+        />
       </View>
-
       {isDeleteMode ? (
         <View
           style={{
---
+++ components/Plan/PlanItemsView.tsx
@@ -0,0 +1,78 @@
+import { Plan } from "@/utils/types";
+import { ScrollView, View } from "react-native";
+import ThemedText from "../Common/ThemedText";
+import Paper from "../Common/Paper";
+import PlanItemView from "./PlanItemView";
+import { Dispatch, SetStateAction } from "react";
+
+function getCategories(plan: Plan) {
+  const allCategories = plan?.items.map((item) => item.category);
+  const uniqueCategories = [...new Set(allCategories)];
+  return uniqueCategories;
+}
+
+interface PlanItemsViewProps {
+  plan: Plan;
+  editItemIdx: number;
+  setEditItemIdx: Dispatch<SetStateAction<number>>;
+  isDeleteMode: boolean;
+}
+
+export default function PlanItemsView({
+  plan,
+  editItemIdx,
+  setEditItemIdx,
+  isDeleteMode,
+}: PlanItemsViewProps) {
+  const categories = getCategories(plan);
+
+  return (
+    <ScrollView contentContainerStyle={{ gap: 8 }}>
+      {categories.map((category) => {
+        return (
+          <View key={category} style={{ gap: 8 }}>
+            <ThemedText size="small" color="gray" style={{ marginLeft: 12 }}>
+              {categories.length == 1 && categories[0] == ""
+                ? "구매 항목"
+                : category == ""
+                ? "분류 없음"
+                : category}
+            </ThemedText>
+            <Paper>
+              {plan?.items.map((planItem, itemIdx) => {
+                if (planItem.checked) return null;
+                if (planItem.category !== category) return null;
+                return (
+                  <PlanItemView
+                    key={planItem.title + planItem.createdAt}
+                    plan={plan}
+                    itemIdx={itemIdx}
+                    needTopBorder={itemIdx == 0}
+                    editItemIdx={editItemIdx}
+                    setEditItemIdx={setEditItemIdx}
+                    isDeleteMode={isDeleteMode}
+                  />
+                );
+              })}
+              {plan?.items.map((planItem, itemIdx) => {
+                if (!planItem.checked) return null;
+                if (planItem.category !== category) return null;
+                return (
+                  <PlanItemView
+                    key={planItem.title + planItem.createdAt}
+                    plan={plan}
+                    itemIdx={itemIdx}
+                    needTopBorder={itemIdx == 0}
+                    editItemIdx={editItemIdx}
+                    setEditItemIdx={setEditItemIdx}
+                    isDeleteMode={isDeleteMode}
+                  />
+                );
+              })}
+            </Paper>
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
+}
