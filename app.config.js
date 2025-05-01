34c590d51c317ee072c2a1122bec376912e3a4e4
codinghalgoyang <codinghalgoyang@gmail.com>
2025-05-01 22:18:36 +0900

    v1.1.4
    Signed-off-by: codinghalgoyang <codinghalgoyang@gmail.com>

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: IS_DEV ? "Shop Plan (dev)" : "Shop Plan",
     slug: "shop-plan",
-    version: "1.1.3",
+    version: "1.1.4",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "myapp",
